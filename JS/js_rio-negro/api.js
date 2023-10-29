2fb74e39ee0dc43d75a373d1095d27bbd09476c8
gaby_0101 <gabyx10666.gmail.com>
2023-10-29 09:43:59 -0300

    deploy001

--- JS/js_rio-negro/api.js
+++ JS/js_rio-negro/api.js
@@ -1,9 +1,11 @@
 
+//========================= API RIO NEGRO ====================
 
-let map = L.map('mi_mapa').setView([-40.7344343, -66.6176455], 15)
+let map_rio_negro = L.map('rio_negro').setView([-40.7344343, -66.6176455], 15)
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            }).addTo(map);
+            }).addTo(map_rio_negro);
 //agregar marcador
-L.marker([-40.7344343, -66.6176455]).addTo(map).bindPopup("cordoba")
-L.marker([-40.7344343, -66.6176455]).addTo(map).bindPopup("cordoba")
+L.marker([-40.7344343, -66.6176455]).addTo(map_rio_negro).bindPopup("rio negro")
+L.marker([-40.7344343, -66.6176455]).addTo(map_rio_negro).bindPopup("rio negro")
+
