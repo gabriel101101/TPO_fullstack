fe69febc314fa2b24f0663dd492a7cac5d7fbaf5
gaby_0101 <gabyx10666.gmail.com>
2023-11-19 15:20:19 -0300

    deploy002

--- JS/js_bsas/apiClima.js
+++ JS/js_bsas/apiClima.js
@@ -20,18 +20,23 @@ function callAPI(city, country){
 }*/
 
 const callAPI = async(city, country) =>{
+
+try{
     
     const apiID= '41d1d7f5c2475b3a16167b30bc4f265c';
     const url= `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiID}`;
 
-    const respuesta =  await fetch(url)
-    .then(data => {
-        return data.json();
-    })
-    .then(dataJSON => {
-        showWeather(dataJSON);
+    const respuesta =  await fetch(url);
+
+    const datos = await respuesta.json();
+
+        showWeather(datos);
         //console.log(dataJSON);
-    })
+    
+} catch{
+    console.log(error);
+}
+
 }
 
 
@@ -42,17 +47,14 @@ function showWeather(data){
     const min = kelvinToCentigrade(temp_min);
     const max = kelvinToCentigrade(temp_max);
 
-    const element = document.createElement('div');
     
-    element.innerHTML = `
+    clima.innerHTML = `
     <h3>Clima en Bs. As.</h3>
     <img class="sol" src="http://openweathermap.org/img/wn/${arr.icon}@2x.png" alt="icon">
     <h2>${degrees}°C</h2>
     <p>Max: ${max}°C</p>
     <p>Min: ${min}°C</p>
     `;
-
-    clima.appendChild(element);
 
 }
 
