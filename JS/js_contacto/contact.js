c01dad4b321caed4c34affb23944a0370aa60f26
gaby_0101 <gabyx10666.gmail.com>
2023-12-17 16:38:14 -0300

    ultimo deploy01

--- JS/js_contacto/contact.js
+++ JS/js_contacto/contact.js
@@ -14,23 +14,34 @@ var form = document.getElementById('formulario');
 var contcheck=0;
 
 
-    form.addEventListener('submit', function(e){
+    form.addEventListener('submit', async function(e){
         e.preventDefault();
-        //console.log("Enviando formulario ...");
         var erroEvent = ['COMPLETE EL CAMPO : '];
         var check = [check1,check2,check3,check4];
+        let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+        //let regex = /^\d+$/;
+        const form = new FormData(this);
+
         
         if(nombre.value === ''){
-            erroEvent.push('"Nombre"');
+            erroEvent.push('< Nombre >');
+            //e.preventDefault();
         }
         if(apellido.value === ''){
-            erroEvent.push('"Apellido"');
+            erroEvent.push('< Apellido >');
+            //e.preventDefault();
         }
-        if(email.value === ''){
-            erroEvent.push('"Email"');
+        if(!regexEmail.test(email.value)){
+            erroEvent.push('< Email >');
+            //e.preventDefault();
         }
+        if (!validarTelefono(telefono.value)) {
+            e.preventDefault(); // Evitar que el formulario se envíe
+            erroEvent.push('< Telefono >');
+          }
         if(message.value === ''){
-            erroEvent.push('"Mensaje"');
+            erroEvent.push('< Mensaje >');
+            //e.preventDefault();
         }
 
         for(let i=0; i<4; i++){
@@ -39,20 +50,38 @@ var contcheck=0;
             }
         }
         if(contcheck===4){
-            erroEvent.push('"Recorrido"');
+            erroEvent.push('< Recorrido >');
+            //e.preventDefault();
         }
-        
-        
-        if(nombre.value === '' || apellido.value === '' || email.value === '' 
-        || message.value === '' || contcheck===4){
+
+        const response = await fetch(this.action,{
+            method:this.method,
+            body:form,
+            headers:{
+                'Accept':'application/json'
+            }
+        })
+        if(response.ok){
+
+            if(nombre.value === '' || apellido.value === '' || !regexEmail.test(email.value) || message.value === '' 
+            || contcheck===4 || !validarTelefono(telefono.value)){
             alert(erroEvent);
+            
+            return false;
+            }else{
+                this.reset()
+                alert('Formulario Enviado...')
+                return true;
+            }   
         }
-
-        
-
-        
         contcheck=0;
-        return true;
+
+        function validarTelefono(telefono) {
+            const regex = /^\d{10}$/;
+            return regex.test(telefono);
+          }
+        
+        //return true;
         
 
     });
